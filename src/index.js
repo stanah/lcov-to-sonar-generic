39b1b354512045277d613c889f798931ab6e0b12
stanah <foo.exe@gmail.com>
2023-06-27 21:38:02 +0900

    refactor

--- src/index.js
+++ src/index.js
@@ -1,66 +1,64 @@
-const lcovParse = require("lcov-parse");
-const xml = require("xmlbuilder");
 const fs = require("fs");
+const lcov = require("lcov-parse");
+const xmlbuilder = require("xmlbuilder");
 
 const INPUT_FILE_PATH = process.env.LCOV_FILE_PATH || "./lcov.info";
 const OUTPUT_FILE_PATH = process.env.XML_FILE_PATH || "./output.xml";
 
-function lcovToSonar(lcovFile) {
-  return new Promise((resolve, reject) => {
-    lcovParse(lcovFile, (err, data) => {
+async function lcovToSonar(inputFilePath) {
+  const lcovData = await new Promise((resolve, reject) => {
+    fs.readFile(inputFilePath, "utf8", (err, data) => {
       if (err) {
-        console.error("Error parsing lcov:", err);
         reject(err);
-        return;
+      } else {
+        lcov(data, (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
       }
-
-      const report = xml.create("coverage", {
-        version: "1.0",
-        encoding: "UTF-8",
-      });
-      report.att("version", "1");
-
-      data.forEach((file) => {
-        const fileNode = report.ele("file", { path: file.file });
-
-        // Handling line coverage
-        file.lines.details.forEach((detail) => {
-          fileNode.ele("lineToCover", {
-            lineNumber: detail.line,
-            covered: detail.hit > 0,
-          });
-        });
-
-        // Handling branch coverage
-        file.branches.details.forEach((detail) => {
-          fileNode.ele("lineToCover", {
-            lineNumber: detail.line,
-            covered: detail.taken > 0,
-            branchesToCover: detail.block, // Assuming 'block' represents branches
-            coveredBranches: detail.taken, // Assuming 'taken' represents covered branches
-          });
-        });
-      });
-
-      resolve(report.end({ pretty: true }));
     });
   });
+
+  const report = xmlbuilder.create("coverage", {
+    version: "1.0",
+    encoding: "UTF-8",
+  });
+  report.att("version", "1");
+
+  lcovData.forEach((file) => {
+    const fileNode = report.ele("file", { path: file.file });
+    file.lines.details.forEach((detail) => {
+      fileNode.ele("lineToCover", {
+        lineNumber: detail.line,
+        covered: detail.hit > 0,
+      });
+    });
+    file.branches.details.forEach((detail) => {
+      fileNode.ele("lineToCover", {
+        lineNumber: detail.line,
+        covered: detail.taken > 0,
+        branchesToCover: detail.block,
+        coveredBranches: detail.taken,
+      });
+    });
+  });
+
+  return report.end({ pretty: true });
 }
 
 async function main() {
-  const xmlString = await lcovToSonar(INPUT_FILE_PATH);
-  fs.writeFile(OUTPUT_FILE_PATH, xmlString, (err) => {
-    if (err) {
-      return console.error("Error writing to file:", err);
-    }
+  try {
+    const xmlString = await lcovToSonar(INPUT_FILE_PATH);
+    await fs.promises.writeFile(OUTPUT_FILE_PATH, xmlString);
     console.log("The file was saved!");
-  });
+  } catch (err) {
+    console.error("Error writing to file:", err);
+  }
 }
 
-main()
-  .catch((e) => {
-    console.log(e);
-  })
-  .then(() => {
-    console.log("end");
-  });
+main().then(() => {
+  console.log("end");
+});
