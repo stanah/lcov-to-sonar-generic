0802e204633506898006ceb5f4c7edba727919a5
stanah <foo.exe@gmail.com>
2023-06-28 00:52:18 +0900

    add cli options

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 const lcov = require("lcov-parse");
 const xmlbuilder = require("xmlbuilder");
-
-const INPUT_FILE_PATH = process.env.LCOV_FILE_PATH || "./lcov.info";
-const OUTPUT_FILE_PATH = process.env.XML_FILE_PATH || "./output.xml";
 
 async function lcovToSonar(inputFilePath) {
   const lcovData = await new Promise((resolve, reject) => {
@@ -51,8 +50,34 @@ async function lcovToSonar(inputFilePath) {
 
 async function main() {
   try {
-    const xmlString = await lcovToSonar(INPUT_FILE_PATH);
-    await fs.promises.writeFile(OUTPUT_FILE_PATH, xmlString);
+    const args = process.argv.slice(2);
+    if (args.length != 2 || args.includes("-h") || args.includes("--help")) {
+      console.log("Usage: lcov-to-sonar-generic [input file] [output file]");
+      console.log("Options:");
+      console.log("  -h, --help    Show help");
+      process.exit(0);
+    }
+
+    const inputFile = args[0];
+    const outputFile = args[1];
+
+    const validFilenameRegex = /^[^<>:"/\\|?*\x00-\x1F]*$/;
+    if (!validFilenameRegex.test(inputFile)) {
+      console.error(`Invalid input file name: ${inputFile}`);
+      process.exit(1);
+    }
+    if (!validFilenameRegex.test(outputFile)) {
+      console.error(`Invalid output file name: ${outputFile}`);
+      process.exit(1);
+    }
+
+    if (!fs.existsSync(inputFile)) {
+      console.error(`Input file ${inputFile} does not exist`);
+      process.exit(1);
+    }
+
+    const xmlString = await lcovToSonar(inputFile);
+    await fs.promises.writeFile(outputFile, xmlString);
     console.log("The file was saved!");
   } catch (err) {
     console.error("Error writing to file:", err);
